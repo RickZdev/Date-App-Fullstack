bc370ba3b0c5038b96ba69c27be9f84e63c303e5
Fred Castañeda <rickzisback@gmail.com>
2022-08-30 18:44:47 +0800

    Moved files to frontend folder

--- frontend/src/components/CustomTextInput.js
+++ frontend/src/components/CustomTextInput.js
@@ -1,13 +1,17 @@
 import { Text, TextInput, View } from 'react-native'
 import React from 'react'
 
-const CustomTextInput = ({ label, keyboardType, additionalStyle, children }) => {
+const CustomTextInput = ({ label, keyboardType, onChangeText, additionalStyle, children }) => {
   return (
     <View className='border-2 rounded-xl border-white'>
       <View className='bg-primary-1 absolute -top-3 left-5 px-2'>
         <Text className='text-primary-0 font-skModernistRegular text-sm'>{label}</Text>
       </View>
-      <TextInput className={`pl-5 py-4 font-skModernistRegular text-white ${additionalStyle}`} style={{ height: 62 }} keyboardType={keyboardType} />
+      <TextInput
+        className={`pl-5 py-4 font-skModernistRegular text-white ${additionalStyle}`}
+        style={{ height: 62 }} keyboardType={keyboardType}
+        onChangeText={onChangeText}
+      />
       {children}
     </View>
   )
